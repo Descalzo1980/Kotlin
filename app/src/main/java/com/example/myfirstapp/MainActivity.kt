a91cb940d268e76b47d33833502af257ebd72d8f
Stas <ragioniere1980@gmail.com>
2022-06-05 19:47:17 +0300

    числовые типы данных

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -10,6 +10,16 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    var a:Byte = 120 // целое число от  -128 до 127 1 байт
+    // одна единица информации (без разницы 0 это или 1) и называеся бит
+    //  8 бит равно Байт
+    var b:Short = 120 // -32768 до 32768 2 байта
+    var c:Int = 120 // по умолчанию Int -2147483648 до 2147483647 4 байта
+    var d:Long = 120 // 8 байт, числа уже ололо
+    var f:Float = 120.8f // 4 байта -3.4*10 в 38 до 3.4*10 в 38 степени
+    var g:Double = 120.5 // 8 байт 5.0 в 10 в 324 степени
+
+
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         setContentView(R.layout.activity_main)
@@ -17,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         val counter2 = counter + 5
         val tvTest = findViewById<TextView>(R.id.tvTest)
         tvTest.text = counter.toString()
+        var test1 = f.toInt()
+        var test2 = f.toString() // это классы
 
     }
 
