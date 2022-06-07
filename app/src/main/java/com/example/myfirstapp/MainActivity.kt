2fc6c4b67bd8ea471013a1d56bd09d692a4be0c2
Stas <ragioniere1980@gmail.com>
2022-06-07 22:11:08 +0300

    кнопочка работает

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -6,27 +6,33 @@ import android.os.PersistableBundle
 import android.util.Log
 import android.util.LogPrinter
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-    var a:Byte = 120 // целое число от  -128 до 127 1 байт
-    // одна единица информации (без разницы 0 это или 1) и называеся бит
-    //  8 бит равно Байт
-    var b:Short = 120 // -32768 до 32768 2 байта
-    var c:Int = 120 // по умолчанию Int -2147483648 до 2147483647 4 байта
-    var d:Long = 120 // 8 байт, числа уже ололо
-    var f:Float = 120.8f // 4 байта -3.4*10 в 38 до 3.4*10 в 38 степени
-    var g:Double = 120.5 // 8 байт 5.0 в 10 в 324 степени
-    var testBool: Boolean = true // это понятно
-    var test: Char = 'A' // это тоже понятно
-    var testString: String = "Привет как дела"
-
-
+    lateinit var tv: TextView
+    lateinit var bt: Button
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         setContentView(R.layout.activity_main)
+
+        //tv.text = "Некий текст" не запстить, экрана нет
+        //var tv = findViewById<TextView>(R.id.tvTest) разные варианты
+        //var tv = findViewById<TextView>(R.id.tvTest) as TextView разные варианты
+        //tv = findViewById<TextView>(R.id.tvTest) разные варианты
+        tv = findViewById(R.id.tvTest)
+        bt = findViewById(R.id.buttonTest)
+        bt.setOnClickListener {
+            tv.text = "Работает"
+        }
     }
+
+    override fun onPause() {
+        super.onPause()
+        tv.text = "И вот опять"
+    }
+
+
 
 }
