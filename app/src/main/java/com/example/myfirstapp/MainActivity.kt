ea56d1680131f38079916851b418db4e2b629de1
Stas <ragioniere1980@gmail.com>
2022-06-15 21:08:58 +0300

    убил час на корректный переход

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 
@@ -17,12 +16,11 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
     }
-    fun onClickGoTest2(view: View){
-
-        val intent = Intent(this, TestActivity2::class.java)
-        startActivity(intent)
+    fun onClickTest1(view: View){
+        val i = Intent( this,  TestActivity1::class.java)
+        i.putExtra("key", "Привет,как дела?")
+        startActivity(i)
     }
 }
-
 
 
--- app/src/main/java/com/example/myfirstapp/TestActivity1.kt
+++ app/src/main/java/com/example/myfirstapp/TestActivity1.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.myfirstapp.databinding.ActivityMainBinding
+import com.example.myfirstapp.databinding.ActivityTest1Binding
 
 class TestActivity1 : AppCompatActivity() {
+
+    private lateinit var bindingClass: ActivityTest1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test1)
-    }
-
-    fun onClickGoMain(view: View){
-
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        bindingClass = ActivityTest1Binding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        val message = intent.getStringExtra("key")
+        bindingClass.tvMessage.text = message
 
     }
-
 }
