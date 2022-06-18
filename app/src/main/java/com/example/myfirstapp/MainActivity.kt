237b40c1ea234b37e1460989fbec85d6853e580a
Stas <ragioniere1980@gmail.com>
2022-06-18 16:52:53 +0300

    регистрационная форма и даже работает

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
@@ -29,16 +30,53 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == Constance.REQUEST_CODE_SING_IN){
+            val l = data?.getStringExtra(Constance.LOGIN)
+            val p = data?.getStringExtra(Constance.PASSWORD)
+            if(login == l && password == p){
+
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(avatarImageID)
+                val textInfo = "$name $second_name $last_name"
+                bindingClass.tvInfo.text = textInfo
+                bindingClass.bHide.visibility = View.GONE
+                bindingClass.eBtn.text = "Выйти"
+            }else{
+
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(R.drawable.figa)
+                bindingClass.tvInfo.text = "Тебя тут не стояло"
+            }
 
         }else if(requestCode == Constance.REQUEST_CODE_SING_UP){
-
+            login = data?.getStringExtra(Constance.LOGIN)!!
+            password = data.getStringExtra(Constance.PASSWORD)!!
+            name = data.getStringExtra(Constance.NAME)!!
+            second_name = data.getStringExtra(Constance.NAME2)!!
+            last_name = data.getStringExtra(Constance.NAME3)!!
+            avatarImageID = data.getIntExtra(Constance.AVATAR_ID, 0)
+            bindingClass.imAvatar.visibility = View.VISIBLE
+            bindingClass.imAvatar.setImageResource(avatarImageID)
+            val textInfo = "$name $second_name $last_name"
+            bindingClass.tvInfo.text = textInfo
+            bindingClass.bHide.visibility = View.GONE
+            bindingClass.eBtn.text = "Выйти"
         }
     }
     fun onClickSingIn(view: View){
 
-        val intent = Intent(this, SingInUpActivity::class.java)
-        intent.putExtra(Constance.SING_STATE, Constance.SING_IN_STATE)
-        startActivityForResult(intent, Constance.REQUEST_CODE_SING_IN)
+        if(bindingClass.imAvatar.isVisible){
+
+            bindingClass.imAvatar.visibility = View.INVISIBLE
+            bindingClass.tvInfo.text = ""
+            bindingClass.bHide.visibility = View.VISIBLE
+            bindingClass.eBtn.text = getString(R.string.sing_in)
+
+        }else{
+            val intent = Intent(this, SingInUpActivity::class.java)
+            intent.putExtra(Constance.SING_STATE, Constance.SING_IN_STATE)
+            startActivityForResult(intent, Constance.REQUEST_CODE_SING_IN)
+        }
+
     }
     fun onClickSingUp(view: View){
         val intent = Intent(this, SingInUpActivity::class.java)
--- app/src/main/java/com/example/myfirstapp/SingInUpActivity.kt
+++ app/src/main/java/com/example/myfirstapp/SingInUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
 import com.example.myfirstapp.databinding.ActivitySingUpInBinding
@@ -31,13 +32,22 @@ class SingInUpActivity : AppCompatActivity() {
     }
 
     fun onClickDone(view: View){
-        if (singState == Constance.SING_IN_STATE) {
+        if (singState == Constance.SING_UP_STATE) {
 
-//            bindingClass.edName.visibility = View.GONE
-//            bindingClass.edName2.visibility = View.GONE
-//            bindingClass.edName3.visibility = View.GONE
-//            bindingClass.bAvatar.visibility = View.INVISIBLE
-
+        val intent = Intent()
+            intent.putExtra(Constance.LOGIN, bindingClass.edLogin.text.toString())
+            intent.putExtra(Constance.PASSWORD, bindingClass.edPassword.text.toString())
+            intent.putExtra(Constance.NAME, bindingClass.edName.text.toString())
+            intent.putExtra(Constance.NAME2, bindingClass.edName2.text.toString())
+            intent.putExtra(Constance.NAME3, bindingClass.edName3.text.toString())
+            if(bindingClass.imAvatar.isVisible)intent.putExtra(Constance.AVATAR_ID, R.drawable.girl)
+            setResult(RESULT_OK, intent)
+            finish()
+        }else if (singState == Constance.SING_IN_STATE){
+            intent.putExtra(Constance.LOGIN, bindingClass.edLogin.text.toString())
+            intent.putExtra(Constance.PASSWORD, bindingClass.edPassword.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
         }
     }
 
